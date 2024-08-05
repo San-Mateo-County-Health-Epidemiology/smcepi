ea200d973f657916208cd30c0d5c73b9d1abbfa1
elizabethjump <ejump@smcgov.org>
2024-08-05 13:41:03 -0700

    adding test scripts for smc_city_clean

--- tests/testthat/test_smc-geographies.R
+++ tests/testthat/test_smc-geographies.R
@@ -1,12 +1,13 @@
 # test smc_city_clean() -------------------------
-test_that("gg_color_title", {
+test_that("smc_city_clean", {
 
   test_df <- data.frame(
-    city = c("S S Francisco", "South San Fransico", "SoSan Franc")
+    city = c("S S Francisco", "South San Fransico", "SoSan Franc", "San Francisco")
   ) %>%
     smc_city_clean(city_col = "city")
 
   expect_equal(3, ncol(test_df))
   expect_equal(c("city_orig", "city_temp", "city_clean"), colnames(test_df))
-  expect_equal("South San Francisco", unique(test_df$city_clean))
+  expect_equal(c("South San Francisco", NA), unique(test_df$city_clean))
+  expect_equal(NA_character_, test_df[test_df$city_orig == "San Francisco", ]$city_clean)
 })
