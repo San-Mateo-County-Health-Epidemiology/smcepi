a4ed88a6b5895056912bfd334945bf20895fa715
elizabethjump <ejump@smcgov.org>
2024-11-19 09:18:40 -0800

    adjusting sort_method inputs + changing name from `get_file` to `get_file_path`

--- R/file-management.R
+++ R/file-management.R
@@ -1,4 +1,4 @@
-get_file <- function(path = NULL, pattern = NULL, sort_method, sort_type = "newest") {
+get_file_path <- function(path = NULL, pattern = NULL, sort_method, sort_type = "newest") {
 
   # get the files that match the path + pattern -----------------
   files <- file.info(list.files(path = path,
@@ -6,14 +6,20 @@ get_file <- function(path = NULL, pattern = NULL, sort_method, sort_type = "newe
                                 full.names = T))
 
   # ensure a valid sort method was inputted ---------------------
-  sort_method <- rlang::arg_match(sort_method, c("ctime","mtime", "atime"))
+  sort_method <- rlang::arg_match(sort_method, c("created date","modified date", "accessed date"))
 
+  switch(sort_method,
+         `created date` = {sort_method = "ctime"},
+         `modified date` = {sort_method = "mtime"},
+         `accessed date` = {sort_method = "atime"})
+
+  # set decreasing variable based on sort type ------------------
   switch(sort_type,
          newest = {sort_type = TRUE},
          oldest = {sort_type = FALSE})
 
+  # sort data + pull file path ----------------------------------
   files["sort_col"] <- files[[sort_method]]
-
   rownames(files[order(files$sort_col, decreasing = sort_type),][1,])
 
 }
