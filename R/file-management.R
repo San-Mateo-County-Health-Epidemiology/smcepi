d31627345782f865c3007c965fd389c358387626
elizabethjump <ejump@smcgov.org>
2024-11-19 17:49:33 -0800

    adding in examples for get_file_path()

--- R/file-management.R
+++ R/file-management.R
@@ -1,4 +1,4 @@
-#' Get file path for a file stored in a folder with multiple versions of the ***
+#' Get file path for a file stored in a folder with multiple versions of a file
 #'
 #' @description
 #' If you have multiple versions of the same data set stored in a folder, this will pull the newest or oldest file path. You can enter a path and/or a pattern to look for.
@@ -22,7 +22,18 @@
 #' @export
 #'
 #' @examples
+#' \dontrun{
 #'
+#' # get the file that was most recently accessed in a directory
+#' get_file_path(sort_method = "accessed date", sort type = "newest")
+#'
+#' # get the first file that was created in a different directory
+#' get_file_path(path = "data", sort_type = "newest")
+#'
+#' # get the most recently modified parquet file in a directory
+#' get_file_path(path = "data", pattern = "*.parquet", sort_method = "modified date", sort_type = "newest")
+#'
+#'}
 get_file_path <- function(path = NULL, pattern = NULL, sort_method = "created date", sort_type = "newest") {
 
   # get the files that match the path + pattern -----------------
