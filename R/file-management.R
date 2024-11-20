b713e1e1334aecf6bd122f37e1611a017c6a0c12
elizabethjump <ejump@smcgov.org>
2024-11-19 17:46:16 -0800

    updating documentation

--- R/file-management.R
+++ R/file-management.R
@@ -1,4 +1,29 @@
-get_file_path <- function(path = NULL, pattern = NULL, sort_method, sort_type = "newest") {
+#' Get file path for a file stored in a folder with multiple versions of the ***
+#'
+#' @description
+#' If you have multiple versions of the same data set stored in a folder, this will pull the newest or oldest file path. You can enter a path and/or a pattern to look for.
+#'
+#' @usage get_file_path(path = NULL,
+#'                      pattern = NULL,
+#'                      sort_method = "created time",
+#'                      sort_type = "newest")
+#'
+#' @param path a string specifying the directory in which you want to look. If you're already in the directory, you can leave this blank
+#' @param pattern a string and/or regular expression specifying which files you're interested in
+#' @param sort_method
+#'   * `"created date"` (the default): sort by the date the file was created
+#'   * `"modified date"`: sort by the date the file was last modified
+#'   * `"accessed date"`: sort by the date the file was last accessed
+#' @param sort_type
+#'   *`"newest"` (the default): get the newest file based on sort method
+#'   *`"oldest"`: get the oldest file based on sort method
+#'
+#' @return the file path as a string
+#' @export
+#'
+#' @examples
+#'
+get_file_path <- function(path = NULL, pattern = NULL, sort_method = "created date", sort_type = "newest") {
 
   # get the files that match the path + pattern -----------------
   files <- file.info(list.files(path = path,
@@ -14,6 +39,8 @@ get_file_path <- function(path = NULL, pattern = NULL, sort_method, sort_type = 
          `accessed date` = {sort_method = "atime"})
 
   # set decreasing variable based on sort type ------------------
+  sort_type <- rlang::arg_match(sort_method, c("newest","oldest"))
+
   switch(sort_type,
          newest = {sort_type = TRUE},
          oldest = {sort_type = FALSE})
