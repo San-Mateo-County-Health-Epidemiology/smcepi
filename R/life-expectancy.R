df6bf2a42f5598f2081f81eade8ac7013f6ac77d
elizabethjump <ejump@smcgov.org>
2024-12-04 15:21:15 -0800

    working through LE steps in base R

--- R/life-expectancy.R
+++ R/life-expectancy.R
@@ -39,8 +39,17 @@ make_life_table <- function(data) {
 
 }
 
+test_data <- data.frame(
+  year = 2020,
+  age_cat = c("0", "1-4", "5-9", "10-14", "15-19","20-24", "25-29", "30-34", "35-39", "40-44", "45-49", "50-54", "55-59", "60-64", "65-69", "70-74", "75-79", "80-84", "85-89", "90+"),
+  deaths = c(101, 10, 16, 15, 65, 102, 164, 218, 268, 301, 427, 631, 999, 1414, 1661, 2244, 2583, 3234, 3841, 6546),
+  population = c(37091, 159844, 226038, 232718, 208813, 187469, 219833, 244278, 268280, 274061, 284629, 289041, 269557, 257883, 230428, 192485, 139779, 85485, 57412, 38668)
+)
+
 # get start age ----
 test_data$start_age <- as.numeric(sub("[\\s\\-]+\\d{1,2}$|\\+$", "", test_data$age_cat))
+test_data$max_age <- ifelse(max(test_data$start_age) == test_data$start_age,
+                            1, 0)
 
 # set interval widths ----
 test_data$int_width <- ifelse(test_data$age_cat == "0", 1,
@@ -65,9 +74,17 @@ test_data$prob_surv <- 1-test_data$prob_dying
 sumprod <- function(x, y) x * y
 test_data$num_alive_int <- Reduce(sumprod, test_data$prob_surv, init = 100000, acc = TRUE)[1:nrow(test_data)]
 
+## CHECK THIS
+test_data$test_num_alive <- aggregate(test_num_alive_int ~ prob_surv, data = test_data)
+
 # get the number dying in the interval ----
-num_dying_int = case_when(row_number() == n() ~ num_alive_int,
-                          TRUE ~ num_alive_int - lead(num_alive_int)),
+test_data$num_dying_int <- ifelse(test_data$max_age == 0,
+                                  ave(test_data$num_alive_int,
+                                      test_data$year,
+                                      FUN = function(x) abs(c(diff(x), 0))),
+                                      test_data$num_alive_int)
+
+
 pers_yrs_lived_int = case_when(row_number() == n() ~ num_alive_int/death_rate,
                                TRUE ~ int_width*(lead(num_alive_int)+(fract_surv*num_dying_int))),
 pers_yrs_lived_past = purrr::accumulate(.x = pers_yrs_lived_int,
