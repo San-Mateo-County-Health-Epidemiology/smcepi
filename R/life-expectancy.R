36bf5a8d1b5e9896ae2ddf27da5908ff4b3424f2
elizabethjump <ejump@smcgov.org>
2024-12-06 12:29:00 -0800

    finished base r code, adding roxygen skeletons

--- R/life-expectancy.R
+++ R/life-expectancy.R
@@ -1,48 +1,13 @@
-make_life_table <- function(data) {
-  life_table <- data %>%
-    mutate(int_width = case_when(age_cat == "0" ~ 1,
-                                 age_cat == "1-4" ~ 4,
-                                 age_cat == "0-4" ~ 5,
-                                 age_cat == "85+" ~ 17.3,
-                                 age_cat == "90+" ~ 12.3,
-                                 TRUE ~ 5),
-           fract_surv = case_when(age_cat == "0" ~ 0.1,
-                                  age_cat == "0-4" ~ 0.02,
-                                  TRUE ~ 0.5),
-           death_rate = deaths/population,
-           prob_dying = int_width*death_rate/(1+int_width*(1-fract_surv)*death_rate),
-           prob_surv = 1-prob_dying,
-           num_alive_int = purrr::accumulate(.x = head(prob_surv, -1),
-                                      .init = 100000,
-                                      .f = `*`),
-           num_dying_int = case_when(row_number() == n() ~ num_alive_int,
-                                     TRUE ~ num_alive_int - lead(num_alive_int)),
-           pers_yrs_lived_int = case_when(row_number() == n() ~ num_alive_int/death_rate,
-                                          TRUE ~ int_width*(lead(num_alive_int)+(fract_surv*num_dying_int))),
-           pers_yrs_lived_past = purrr::accumulate(.x = pers_yrs_lived_int,
-                                            .f = `+`,
-                                            .dir = "backward"),
-           obs_le_int = pers_yrs_lived_past/num_alive_int,
-           sample_var = case_when(row_number() == n() ~ (death_rate*(1-death_rate)/population),
-                                  deaths == 0 ~ 0,
-                                  TRUE ~ (prob_dying^2*(1-prob_dying)/deaths)),
-           weighted_var = case_when(row_number() == n() ~ (num_alive_int^2)/death_rate^4*sample_var,
-                                    TRUE ~ (num_alive_int^2)*((1-fract_surv)*int_width+lead(obs_le_int))^2*sample_var),
-           sample_var_pers_yrs = purrr::accumulate(.x = weighted_var,
-                                            .f = `+`,
-                                            .dir = "backward"),
-           sample_var_obs_le = sample_var_pers_yrs/num_alive_int^2,
-           ci_low_95 = round(obs_le_int-(1.96*sqrt(sample_var_obs_le)), 1),
-           ci_high_95 = round(obs_le_int+(1.96*sqrt(sample_var_obs_le)), 1))
-
-  life_table
-
-}
-
-
-
-# get start age for each year ----
-le_fun <- function(x) {
+#' life table
+#'
+#' this is the function called in the `make_life_table()`.
+#'
+#' @param x a dataframe
+#'
+#' @return
+#'
+#' @examples
+life_table <- function(x) {
 
   start_age = as.numeric(sub("[\\s\\-]+\\d{1,2}$|\\+$", "", x[, c("age_cat")]))
   max_age = ifelse(max(start_age) == start_age, 1, 0)
@@ -66,88 +31,48 @@ le_fun <- function(x) {
 
   pers_yrs_lived_past = rev(cumsum(rev(pers_yrs_lived_int)))
 
+  obs_le_int = pers_yrs_lived_past/num_alive_int
+
+  sample_var = ifelse(max_age == 1, (death_rate*(1-death_rate)/x[, c("population")]),
+                      ifelse(x[, c("deaths")] == 0, 0, (prob_dying^2*(1-prob_dying)/x[, c("deaths")])))
+  weighted_var = ifelse(max_age == 1, (num_alive_int^2)/death_rate^4*sample_var,
+                        (num_alive_int^2)*((1-fract_surv)*int_width+lead(obs_le_int))^2*sample_var)
+
+  sample_var_pers_yrs = rev(cumsum(rev(weighted_var)))
+  sample_var_obs_le = sample_var_pers_yrs/num_alive_int^2
+  ci_low_95 = round(obs_le_int-(1.96*sqrt(sample_var_obs_le)), 1)
+  ci_high_95 = round(obs_le_int+(1.96*sqrt(sample_var_obs_le)), 1)
+
+
   cbind(x, start_age, max_age, int_width, fract_surv, death_rate, prob_dying, prob_surv,
-        num_alive_int, num_dying_int, pers_yrs_lived_int, pers_yrs_lived_past)
+        num_alive_int, num_dying_int, pers_yrs_lived_int, pers_yrs_lived_past,
+        obs_le_int, sample_var, weighted_var, sample_var_pers_yrs, sample_var_obs_le, ci_low_95, ci_high_95)
 
 }
 
-test_data <- data.frame(
-  year = c(rep(2020, 20), rep(2021, 20)),
-  age_cat = rep(c("0", "1-4", "5-9", "10-14", "15-19","20-24", "25-29", "30-34", "35-39", "40-44", "45-49", "50-54", "55-59", "60-64", "65-69", "70-74", "75-79", "80-84", "85-89", "90+"), 2),
-  deaths = c(101, 10, 16, 15, 65, 102, 164, 218, 268, 301, 427, 631, 999, 1414, 1661, 2244, 2583, 3234, 3841, 6546, 114, 15, 14, 16, 59, 108, 149, 192, 231, 265, 403, 620, 1010, 1353, 1692, 2083, 2495, 3126, 3812, 6443),
-  population = c(37091, 159844, 226038, 232718, 208813, 187469, 219833, 244278, 268280, 274061, 284629, 289041, 269557, 257883, 230428, 192485, 139779, 85485, 57412, 38668, 38483, 165990, 228426, 231830, 208844, 189191, 219408, 245057, 267273, 274420, 288485, 286389, 271027, 254730, 222002, 179366, 126073, 85853, 57803, 38744)
-)
+#' Title
+#'
+#' @param data
+#' @param year_col
+#' @param age_cat_col
+#' @param deaths_col
+#' @param population_col
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+#'
+make_life_table <- function(data, year_col = "year", age_cat_col = "age_cat", deaths_col = "deaths", population_col = "population") {
 
-test_data <- do.call(rbind, by(test_data, test_data[, c("year")], le_fun))
+  # allow user to specify names of variables
+  names(df)[names(df) == year_col] <- 'year'
 
+  # check that all of our columns are in the data set
+  stopifnot(all(c("year", "age_cat", "deaths", "population") %in% colnames(data)))
 
-pers_yrs_lived_int = case_when(row_number() == n() ~ num_alive_int/death_rate,
-                               TRUE ~ int_width*(lead(num_alive_int)+(fract_surv*num_dying_int))),
-pers_yrs_lived_past = purrr::accumulate(.x = pers_yrs_lived_int,
-                                        .f = `+`,
-                                        .dir = "backward"),
-obs_le_int = pers_yrs_lived_past/num_alive_int,
-sample_var = case_when(row_number() == n() ~ (death_rate*(1-death_rate)/population),
-                       deaths == 0 ~ 0,
-                       TRUE ~ (prob_dying^2*(1-prob_dying)/deaths)),
-weighted_var = case_when(row_number() == n() ~ (num_alive_int^2)/death_rate^4*sample_var,
-                         TRUE ~ (num_alive_int^2)*((1-fract_surv)*int_width+lead(obs_le_int))^2*sample_var),
-sample_var_pers_yrs = purrr::accumulate(.x = weighted_var,
-                                        .f = `+`,
-                                        .dir = "backward"),
-sample_var_obs_le = sample_var_pers_yrs/num_alive_int^2,
-ci_low_95 = round(obs_le_int-(1.96*sqrt(sample_var_obs_le)), 1),
-ci_high_95 = round(obs_le_int+(1.96*sqrt(sample_var_obs_le)), 1))
+  test_data <- do.call(rbind, by(test_data, test_data[, c("year")], make_life_table))
 
-
-library(dplyr)
-test_le <- test_data %>%
-  select(year, age_cat, deaths, population) %>%
-  mutate(start_age = as.numeric(stringr::str_extract(age_cat, "^\\d{1,2}"))) %>% # start age makes sure the age groups sort correctly
-  group_by(year) %>% # you can add other grouping variables (race, sex, etc here)
-  arrange(start_age) %>%
-  make_life_table() %>%
-  ungroup() %>%
-  arrange(year, start_age)
-
-# compare!
-test_le$start_age == test_data$start_age
-test_le$int_width == test_data$int_width
-test_le$fract_surv == test_data$fract_surv
-test_le$death_rate == test_data$death_rate
-test_le$prob_dying == test_data$prob_dying
-test_le$prob_surv == test_data$prob_surv
-test_le$num_alive_int == test_data$num_alive_int
-test_le$num_dying_int == test_data$num_dying_int
-test_le$pers_yrs_lived_int == test_data$pers_yrs_lived_int
-test_le$pers_yrs_lived_past == test_data$pers_yrs_lived_past
-
-
-
-# old code!
-
-test_data$start_age <- as.numeric(sub("[\\s\\-]+\\d{1,2}$|\\+$", "", test_data$age_cat))
-
-# get max age for each year
-max_ages <- aggregate(start_age ~ year, max, data = test_data)
-colnames(max_ages) <- c("year", "max_age")
-test_data <- merge(test_data, max_ages, all.x = T)
-test_data$max_age <- ifelse(test_data$max_age == test_data$start_age, 1, 0)
-
-# set interval widths ----
-test_data$int_width <- ifelse(test_data$age_cat == "0", 1,
-                              ifelse(test_data$age_cat == "1-4", 4,
-                                     ifelse(test_data$age_cat == "0-4", 5,
-                                            ifelse(test_data$age_cat == "85+", 17.3,
-                                                   ifelse(test_data$age_cat == "90+", 12.3, 5)))))
-# set fraction of last age interval survivied ----
-test_data$fract_surv <- ifelse(test_data$age_cat == "0", 0.1,
-                               ifelse(test_data$age_cat == "0-4", 0.02, 0.5))
-
-test_data <- data.frame(
-  year = 2020,
-  age_cat = c("0", "1-4", "5-9", "10-14", "15-19","20-24", "25-29", "30-34", "35-39", "40-44", "45-49", "50-54", "55-59", "60-64", "65-69", "70-74", "75-79", "80-84", "85-89", "90+"),
-  deaths = c(101, 10, 16, 15, 65, 102, 164, 218, 268, 301, 427, 631, 999, 1414, 1661, 2244, 2583, 3234, 3841, 6546),
-  population = c(37091, 159844, 226038, 232718, 208813, 187469, 219833, 244278, 268280, 274061, 284629, 289041, 269557, 257883, 230428, 192485, 139779, 85485, 57412, 38668)
-)
+}
 
