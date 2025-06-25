76b2b6cbb32dd01582806754e678d1a6591f623d
Eamonn Hartmann <ehartmann@smcgov.org>
2025-06-25 15:13:37 -0700

    initial draft of sd calculation

---
+++ R/sd-calculation.R
@@ -0,0 +1,23 @@
+#' Standard deviation calculation for a proportion
+#' @description Use this function to calculate the standard deviation of a proportion (e.g. rate). The standard deviation is a measure of spread that represents how far the population proportion is from the sample proportions on average.
+#'
+#' @param num This is the numerator of the proportion.
+#' @param denom This is the denominator of the proportion.
+#'
+#' @return The standard deviation of a proportion.
+#' @export
+#'
+#' @examples
+#' sd(5,20)
+#'
+sd <- function(num, denom) {
+
+  stopifnot(is.numeric(num) & is.numeric(denom))
+
+  prop <- num / denom
+
+  sd <- round(sqrt(prop*(1-prop)/denom), digits = 3)
+
+  return(sd)
+
+}
