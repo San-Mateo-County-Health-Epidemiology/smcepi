5998b5ec7c826d8078ddb34b19aac04aad6006bc
Eamonn Hartmann <ehartmann@smcgov.org>
2025-10-01 15:59:11 -0700

    updating tests

--- tests/testthat/test_sd-calculation.R
+++ tests/testthat/test_sd-calculation.R
@@ -7,8 +7,8 @@ test_that("prop_sd", {
     num <- 50
   denom <- 2000
 
-  expect_equal(class(sd_df$num), "numeric")
-  expect_equal(class(sd_df$denom), "numeric")
-  expect_equal(prop_sd(sd_df$num, sd_df$denom), 0.003, tolerance=1e-2)
+  expect_equal(class(num), "numeric")
+  expect_equal(class(denom), "numeric")
+  expect_equal(prop_sd(num, denom), 0.003, tolerance=1e-2)
 
 })
